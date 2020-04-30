4446d981ea9024c291f8651f73fe7ddd4b8b2fcc
marcosfelipp <mfelipe11@yahoo.com.br>
2020-04-30 15:09:13 -0300

    Update template

--- telegram_notifications/image_generator.py
+++ telegram_notifications/image_generator.py
@@ -1,20 +1,20 @@
 import imgkit
 
 
-string = "<!DOCTYPE html><html lang=\"pt-br\"><head><link rel=\"stylesheet\" href=\"template.css\"></head><body><div class=\"box\"><img src=\"OIBR3.png\" width=\"100\" height=\"100\">bbabababababb</div></body>"
+string = "<!DOCTYPE html><html lang=\"pt-br\"><head><link rel=\"stylesheet\" href=\"template.css\"><meta charset=\"utf-8\"></head><body><div class=\"header-box\"><h3>Recomendação de #valor-t</h3></div><div class=\"box\"><div class=\"img-box\"><img src=\"#valor-ativo.png\"></div><div class=\"stock-name\"><h2>#valor-ativo</h2></div><table><tr><th></th><th>Valor</th></tr><tr><td><h3>Entrada: </h3></td><td><h3>#valor-entrada</h3></td></tr><tr><td><h3>Objetivo: </h3></td><td><h3>#valor-objetivo</h3></td></tr><tr><td><h3>Stop: </h3></td><td><h3>#valor-stop</h3></td></tr><tr><td><h3>Lucro: </h3></td><td><h3>#valor-lucro</h3></td></tr></table></div></body>"
 
 
 def generate_html(ativo, tipo, obetivo, entrada, stop_loss):
-
+    new_str = string.replace("#valor-t", tipo).replace("#valor-entrada", entrada).replace("#valor-objetivo", obetivo).replace("#valor-stop", stop_loss).replace("#valor-ativo", ativo)
     with open("template_teste.html", 'w') as f:
-        f.write(string)
+        f.write(new_str)
 
 
 def generate_image(ativo, tipo, obetivo, entrada, stop_loss):
     options = {
         'format': 'png',
-        'crop-h': '200',
-        'crop-w': '200',
+        'crop-h': '500',
+        'crop-w': '320',
         'encoding': "UTF-8",
     }
     generate_html(ativo, tipo, obetivo, entrada, stop_loss)
@@ -22,3 +22,5 @@ def generate_image(ativo, tipo, obetivo, entrada, stop_loss):
     imgkit.from_file('template_teste.html', 'out.png', options=options)
 
 
+if __name__ == "__main__":
+    generate_image("OIBR3", "Venda", "0,54", "0,50", "0,40")
